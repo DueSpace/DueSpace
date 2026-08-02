fcda730f90ac5a754ad28f709a9400a566ef1638
MildstoneD <98883461+MildstoneD@users.noreply.github.com>
2026-08-02 06:13:59 +0100

    Change heading from h1 to span for product idea

--- frontend/src/components/StructuredProductTeams.jsx
+++ frontend/src/components/StructuredProductTeams.jsx
@@ -57,8 +57,8 @@ export const StructuredProductTeams = () => {
       {/* Heading */}
       <div className="md:grid md:grid-cols-[.92fr_1.08fr] md:items-center md:gap-12">
         <div>
-          <h1 className="mt-4 text-4xl font-bold leading-tight w-70 md:w-full text-black md:text-5xl">
-            Your Product Idea Is Ready.</h1>
+          <span className="mt-4 text-4xl font-bold leading-tight w-70 md:w-full text-black md:text-5xl">
+            Your Product Idea Is Ready.</span>
             <span className="font-semibold text-3xl w-70 md:w-full md:text-4xl">
               Your Tech Team Should Be Too.
             </span>
