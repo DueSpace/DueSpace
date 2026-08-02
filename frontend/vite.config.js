725ab4795a0ab524ad52231e8027046ed07e6ac3
MildstoneD <Mildstone54@gmail.com>
2026-08-02 01:18:34 +0100

    sitemaps

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import Sitemaps from "vite-plugin-sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    Sitemaps({
-      hostname: "https://www.duespace.com",
-      readable: true,
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
 });
